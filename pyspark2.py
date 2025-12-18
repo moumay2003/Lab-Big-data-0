80d594cc10cf7bcc5980ac4b64c5645f11819318
Mouad <mouadmoulayrachid@gmail.com>
2025-12-18 23:25:30 +0100

    first

--- pyspark2.py
+++ pyspark2.py
@@ -1,4 +1,4 @@
-import pyspark
+import pyspark2
 from pyspark.sql import SparkSession
 
 spark=SparkSession.builder.master("local").appName("MyApp").getOrCreate()
@@ -7,3 +7,4 @@ data=sc.textFile("hdfs://hadoop-master:9000/user/root/input/alice.txt")
 words=data.flatMap(lambda line: line.split(" "))
 wordCounts=words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 wordCounts.saveAsTextFile("hdfs://hadoop-master:9000/user/root/output/wordcount")
+spa
